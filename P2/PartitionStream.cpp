d18b2f0eae79638813d2274748df31757478cfcf
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-10-15 14:09:26 -0700

    in-progress implementation

--- P2/PartitionStream.cpp
+++ P2/PartitionStream.cpp
@@ -6,17 +6,59 @@
 #include <cstring>
 #include <stdexcept>
 
-PartitionStream::PartitionStream(int capacity, const MsgStream* streams, const char keys[][30])
-{
+PartitionStream::PartitionStream(int initialCapacity, const MsgStream* streams)
+    : partitionCount(0), capacity(verifyCapacity(initialCapacity)) {    
+        partitions = new Partition[capacity];
 
+        for (int i = 0; i < capacity; i++)
+        {
+            partitions[i].stream = new MsgStream(streams[i]);
+            partitions[i].key = nullptr;
+        }
+}
+
+PartitionStream::PartitionStream(const PartitionStream& other)
+    : partitionCount(other.partitionCount), capacity(other.capacity) {
+        partitions = new Partition[capacity];
+
+        for (int i = 0; i < partitionCount; i++)
+        {
+            partitions[i].key = new char[strlen(other.partitions[i].key) + 1];
+            strcpy(partitions[i].key, other.partitions[i].key);
+            partitions[i].stream = new MsgStream(*other.partitions[i].stream);
+        }
+}
+
+PartitionStream& PartitionStream::operator=(const PartitionStream& other)
+{
+    if (this == &other) return *this;
+
+    for (int i = 0; i < partitionCount; i++)
+    {
+        delete[] partitions[i].key;
+        delete partitions[i].stream;
+    }
+    delete[] partitions;
+
+    partitionCount = other.partitionCount;
+    capacity = other.capacity;
+    partitions = new Partition[capacity];
+
+    for (int i = 0; i < partitionCount; i++) 
+    {
+        partitions[i].key = new char[strlen(other.partitions[i].key) + 1];
+        strcpy(partitions[i].key, other.partitions[i].key);
+        partitions[i].stream = new MsgStream(*other.partitions[i].stream);
+    }
+    return *this;
 }
 
 PartitionStream::~PartitionStream()
 {
     for (int i = 0; i < partitionCount; i++)
     {
-        delete[] partition[i].key;
-        delete partition[i].stream;
+        delete[] partitions[i].key;
+        delete partitions[i].stream;
     }
-    delete[] partition;
+    delete[] partitions;
 }
