60182e87218ba1d1c2d6702227d408f4aa31b95c
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-10-14 20:26:37 -0700

    in progress implementation

--- P2/PartitionStream.cpp
+++ P2/PartitionStream.cpp
@@ -1,2 +1,22 @@
 // Saxton Van Dalsen
 // 10/15/2024
+
+#include "PartitionStream.h"
+#include "MsgStream.h"
+#include <cstring>
+#include <stdexcept>
+
+PartitionStream::PartitionStream(int capacity, const MsgStream* streams, const char keys[][30])
+{
+
+}
+
+PartitionStream::~PartitionStream()
+{
+    for (int i = 0; i < partitionCount; i++)
+    {
+        delete[] partition[i].key;
+        delete partition[i].stream;
+    }
+    delete[] partition;
+}
