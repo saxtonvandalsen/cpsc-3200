05371066c1a553a7ca73379c49fc722c604fd308
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-04 19:58:02 -0800

    working on declaration

--- P4/PartitionStream.h
+++ P4/PartitionStream.h
@@ -5,13 +5,25 @@
 #define PARTITIONSTREAM_H
 
 #include "MsgStream.h"
+#include <memory>
+#include <string>
+
+using namespace std;
 
 class PartitionStream {
 
     private:
+        unique_ptr<MsgStream[]> streams;
+        unique_ptr<int[]> keys;
+        int capacity;
+        int streamCount;
+        int operationCount;
+
+        int findPartitionIndex(const string& key) const;
 
     public:
-
+        PartitionStream(int initialCapacity);
+        
 };
 
 #endif
