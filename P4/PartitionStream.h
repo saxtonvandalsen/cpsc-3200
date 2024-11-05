bf03d41a58eb208659116a98fb8e2772a1596d3b
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-05 12:53:12 -0800

    draft of declarations

--- P4/PartitionStream.h
+++ P4/PartitionStream.h
@@ -7,10 +7,14 @@
 #include "MsgStream.h"
 #include <memory>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 class PartitionStream {
+
+    // Class invariant:
+    // 
 
     private:
         unique_ptr<MsgStream[]> streams;
@@ -19,11 +23,29 @@ class PartitionStream {
         int streamCount;
         int operationCount;
 
+        PartitionStream(const PartitionStream& other);
+        PartitionStream& operator=(const PartitionStream& other);
+        PartitionStream(PartitionStream&& other) noexcept;
+        PartitionStream& operator=(PartitionStream&& other) noexcept;
+
         int findPartitionIndex(const string& key) const;
+        bool validatePartitionKey(const string& key) const;
+        int verifyCapacity(int initialCapacity);
+        bool operationLimitReached();
+        bool isFull();
+        bool isValidMessage(const string& message) const;
 
     public:
         PartitionStream(int initialCapacity);
-        
+
+        void writeMessage(string& message);
+        unique_ptr<MsgStream[]> readMessage(int startRange, int endRange);
+        int getCapacity();
+        int getPartitionCount();
+        unique_ptr<int[]> getPartitionKeys();
 };
+
+// Implementation invariant:
+//
 
 #endif
