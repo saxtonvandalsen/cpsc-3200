3c211d0a3471fae3a8a4a0564f2d61636a7e8ab4
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-08 11:11:15 -0800

    created foundation

--- P4/DurableStream.h
+++ P4/DurableStream.h
@@ -1,2 +1,27 @@
 // Saxton Van Dalsen
 // 11/14/2024
+
+#ifndef DURABLESTREAM_H
+#define DURABLESTREAM_H
+
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+class DurableStream
+{
+    // Class invariant:
+    //
+
+    private:
+
+    public:
+
+
+    // Implementation invariant:
+    //
+};
+
+#endif
