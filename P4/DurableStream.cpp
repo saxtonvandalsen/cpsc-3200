a934861c2032a94f253457740637c3fa8bde1ff5
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-09 19:47:33 -0800

    initial implementation

--- P4/DurableStream.cpp
+++ P4/DurableStream.cpp
@@ -2,10 +2,57 @@
 // 11/14/2024
 
 #include "DurableStream.h"
+#include "MsgStream.h"
 
 #include <memory>
 #include <string>
+#include <fstream>
 #include <stdexcept>
 
 using namespace std;
 
+DurableStream::DurableStream(int capacity, const string& filePath) : appendCounter(0), MsgStream(capacity)
+{
+    ifstream inFile(filePath);
+    if (inFile.is_open())
+    {
+        string line;
+        while (getline(inFile, line))
+        {
+            this->appendMessage(line);
+        }
+        inFile.close();
+    }
+
+    outFile.open(filePath, ios::app);
+    if (!outFile.is_open())
+    {
+        throw runtime_error("Failed to open file for writing.");
+    }
+}
+
+void DurableStream::syncMessages()
+{
+    initialState = make_unique<string[]>(capacity);
+
+    ifstream inFile(filePath);
+    if (inFile.is_open())
+    {
+        string line;
+        int index = 0;
+        while (getline(inFile, line) && index < capacity)
+        {
+            if (isValidMessage(line))
+            {
+                MsgStream::appendMessage(line);
+                initialState[index++] = line;
+            }
+        }
+        inFile.close();
+    }
+}
+
+bool DurableStream::isValidMessage(const string& message) const
+{
+    return message != "" && message.length() <= MAX_STRING_LENGTH;
+}
