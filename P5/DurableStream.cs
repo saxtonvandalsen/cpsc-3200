2fa0814d0ce2f952c1f927206c99776d7cb68563
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-19 19:47:39 -0800

    foundation & invariants

--- P5/DurableStream.cs
+++ P5/DurableStream.cs
@@ -1,3 +1,19 @@
 // Saxton Van Dalsen
 // 12/3/2024
 
+using System;
+using Systems.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DurableStreamLibrary
+{
+    // Class invariant
+    // -
+    public class DurableStream : MsgStream
+    {
+    
+    }
+    // Implementation invariant:
+    // -
+}
