9a49a404090f78c665e1f89c02bd40b0d74f6f4e
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-11-26 14:52:02 -0800

    added driver/main

---
+++ P5/P5.cs
@@ -0,0 +1,15 @@
+// Saxton Van Dalsen
+// 12/3/2024
+
+using System;
+using PartitionStreamLibrary;
+using MsgStreamLibrary;
+using DurableStreamLibrary;
+
+class program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
